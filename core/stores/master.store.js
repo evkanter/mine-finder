ef8e63fc6dc9093e30b5b4c6a5adb0a1505b78a2
Evan Kanter <ekanter@sapient.com>
2017-07-25 13:08:32 -0400

    remove comments

--- core/stores/master.store.js
+++ core/stores/master.store.js
@@ -4,19 +4,10 @@ import { gameStore } from './game.store';
 import { menuStore } from './menu.store';
 
 class MasterStore {
-
 	constructor() {
 		this.gameStore = gameStore;
 		this.menuStore = menuStore;
 	}
-
-/** 
-    index = 0;
-    list2 = [1,2,3,4,5];
-    list = observable([]);
-    gameMode = observable('Normal');
-    bigList = observable([1, 2, 4]);
-*/
 }
 
 
